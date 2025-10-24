ebee568553bb9625d8b532680b040648075e1e7f
yakubenkovladyslav-glitch <yakubenkovladyslav@gmail.com>
2025-10-24 18:27:42 -0400

    .

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 function openGame(url) {
-  document.getElementById('game-frame').src = url;
+  if(url === '#') {
+    alert("Ce jeu n'est pas encore disponible !");
+    return;
+  }
+
+  const iframe = document.getElementById("game-frame");
+  const gameContainer = document.getElementById("game-container");
+
+  iframe.src = url;
+  gameContainer.style.display = "flex";
 }
